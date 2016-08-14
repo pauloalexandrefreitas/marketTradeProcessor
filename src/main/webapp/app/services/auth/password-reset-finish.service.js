78451fbc2c5c182c4ab52738d9852f9f860f6942
Paulo Alexandre Freitas <a2freitas@gmail.com>
2016-08-14 14:35:08 +0100

    initial commit

---
+++ src/main/webapp/app/services/auth/password-reset-finish.service.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('marketTradeProcessorApp')
+        .factory('PasswordResetFinish', PasswordResetFinish);
+
+    PasswordResetFinish.$inject = ['$resource'];
+
+    function PasswordResetFinish($resource) {
+        var service = $resource('api/account/reset_password/finish', {}, {});
+
+        return service;
+    }
+})();
